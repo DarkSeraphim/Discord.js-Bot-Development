3f8ce2d1776bed6e47c21e07ea233029cc149084
Mark Hendriks <datfireblast709@gmail.com>
2018-02-24 22:56:04 +0000

    Change some " to '

--- episode_5_code/index.js
+++ episode_5_code/index.js
@@ -1,21 +1,22 @@
-const botconfig = require("./botconfig.json");
-const tokenfile = require("./token.json");
-const Discord = require("discord.js");
-const fs = require("fs");
+const botconfig = require('./botconfig.json');
+const tokenfile = require('./token.json');
+const Discord = require('discord.js');
+const fs = require('fs');
 const bot = new Discord.Client({disableEveryone: true});
 const commands = new Discord.Collection();
-const LOG_ERROR = error => console.log(error);
+const {Constants: {Events}} = Discord;
+dconst LOG_ERROR = error => console.log(error);
 
-fs.readdir("./commands/", (err, files) => {
+fs.readdir('./commands/', (err, files) => {
 
   if(err) return console.log(err);
 
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
-    return console.log("Couldn't find commands.");
+  let jsfile = files.filter(f => f.split('.').pop() === 'js')
+  if (jsfile.length <= 0) {
+    return console.log('Couldn\'t find commands.');
   }
 
-  jsfile.forEach((f, i) =>{
+  jsfile.forEach((f, i) => {
     let props = require(`./commands/${f}`);
     if (!props.help || !props.help.name || !props.help.run) {
       return console.log(`${f} is not in the correct format!`);
@@ -27,7 +28,7 @@ fs.readdir("./commands/", (err, files) => {
 });
 
 
-bot.on("ready", () => {
+bot.on(Events.READY, () => {
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
 
   bot.user.setActivity("tutorials on TSC", {type: "WATCHING"}).catch(LOG_ERROR);
@@ -35,12 +36,12 @@ bot.on("ready", () => {
   //bot.user.setGame("on SourceCade!");
 });
 
-bot.on("message", message => {
+bot.on(Events.MESSAGE_CREATE, message => {
   if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
+  if(message.channel.type === 'dm') return;
 
   let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
+  let messageArray = message.content.split(' ');
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
